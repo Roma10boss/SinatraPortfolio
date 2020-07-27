291dc63a1ee571bd58617aaf9c6bb3403d20946c
Romario Gustave <romagustave10@gmail.com>
2020-07-27 23:48:24 +0000

    1 errror

---
+++ app/controller/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicatioController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    #enable :sessions
+    #set :session_secret, 'carcollection'
+  end
+
+  get '/' do
+    "hello world"
+  end
+end
